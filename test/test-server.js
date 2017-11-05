c92835fb1571bf57ce4bc09e71cb86945d2e3b62
Ykat <ykat@Yekaterinas-MacBook-Pro.local>
2017-11-05 11:14:52 -0800

    tests

--- test/test-server.js
+++ test/test-server.js
@@ -12,3 +12,81 @@ const should = chai.should();
 // in our tests.
 // see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
+
+
+describe('Blog API', function() {
+
+  // Before our tests run, we activate the server. Our `runServer`
+  // function returns a promise, and we return the that promise by
+  // doing `return runServer`. If we didn't return a promise here,
+  // there's a possibility of a race condition where our tests start
+  // running before our server has started.
+  before(function() {
+    return runServer();
+  });
+
+  // although we only have one test module at the moment, we'll
+  // close our server at the end of these tests. Otherwise,
+  // if we add another test module that also has a `before` block
+  // that starts our server, it will cause an error because the
+  // server would still be running from the previous tests.
+  after(function() {
+    return closeServer();
+  });
+
+  // test strategy:
+  //   1. make request to `/shopping-list`
+  //   2. inspect response object and prove has right code and have
+  //   right keys in response object.
+  it('should list items on GET', function() {
+    // for Mocha tests, when we're dealing with asynchronous operations,
+    // we must either return a Promise object or else call a `done` callback
+    // at the end of the test. The `chai.request(server).get...` call is asynchronous
+    // and returns a Promise, so we just return it.
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+
+        // because we create three items on app load
+        res.body.length.should.be.at.least(1);
+        // each item should be an object with key/value pairs
+        // for `id`, `name` and `checked`.
+        const expectedKeys = ['title', 'content', 'author'];
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.include.keys(expectedKeys);
+        });
+      });
+  });
+
+  // test strategy:
+  //  1. make a POST request with data for a new item
+  //  2. inspect response object and prove it has right
+  //  status code and that the returned object has an `id`
+it('should add a blog post on POST', function() {
+    const newPost = {
+      title: 'Lorem ip some',
+      content: 'foo foo foo foo',
+      author: 'Emma Goldman'
+    };
+    const expectedKeys = ['id', 'publishDate'].concat(Object.keys(newPost));
+
+    return chai.request(app)
+      .post('/blog-posts')
+      .send(newPost)
+      .then(function(res) {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.all.keys(expectedKeys);
+        res.body.title.should.equal(newPost.title);
+        res.body.content.should.equal(newPost.content);
+        res.body.author.should.equal(newPost.author)
+      });
+  });
+
+
+});
+
