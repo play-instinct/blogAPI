39fbd809a3f1283009342fba59f3b8f2182bcc83
Ykat <ykat@Yekaterinas-MacBook-Pro.local>
2017-11-05 10:41:47 -0800

    add git ignore and ran npm install

---
+++ test/test-server.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+// this lets us use *should* style syntax in our tests
+// so we can do things like `(1 + 1).should.equal(2);`
+// http://chaijs.com/api/bdd/
+const should = chai.should();
+
+// This let's us make HTTP requests
+// in our tests.
+// see: https://github.com/chaijs/chai-http
+chai.use(chaiHttp);
